bc707917d9a4828100a4b387c7fc1971b5ac9e36
minato243 <thaodt.ict@gmail.com>
2024-09-04 00:59:48 +0700

    add build android

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -10,5 +10,13 @@ public class BuildScript
         BuildPipeline.BuildPlayer(scenes,"../WindowsBuild/PerfoceExample.exe", 
             BuildTarget.StandaloneWindows, BuildOptions.None);
     }
+	
+	static void BuildAndroid()
+    {
+        string[] scenes = { "Assets/Scenes/SampleScene.unity"};
+
+        BuildPipeline.BuildPlayer(scenes,"../Android/Test.apk", 
+            BuildTarget.Android, BuildOptions.None);
+    }
 
 }
